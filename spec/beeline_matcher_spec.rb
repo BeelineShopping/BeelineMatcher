e20ae50b7ffbb0f886372c88302e965ad525dac6
Vilyan01 <bheller2010@live.com>
2016-04-29 15:37:33 -0700

    Added tests

--- spec/beeline_matcher_spec.rb
+++ spec/beeline_matcher_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe BeelineMatcher do
+	before(:each) do
+		@matcher = BeelineMatcher::Matcher.new("http://food2fork.com/view/35120")
+	end
   it 'has a version number' do
     expect(BeelineMatcher::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'creates a new object' do
+  	expect(@matcher).not_to be nil
+  end
+
+  it 'retrieves ingredients from food2fork' do
+  	expect(@matcher.get_ingredients.length).to be 6
   end
 end
