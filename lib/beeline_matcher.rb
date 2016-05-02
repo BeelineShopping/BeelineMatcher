10710f644ab9c3afd90624613e1aa5d01a8b4aba
Vilyan01 <bheller2010@live.com>
2016-05-02 13:41:28 -0700

    Added helper class

--- lib/beeline_matcher.rb
+++ lib/beeline_matcher.rb
@@ -4,4 +4,4 @@ require "beeline_matcher/ingredients"
 require "beeline_matcher/model"
 require "beeline_matcher/error"
 require "beeline_matcher/parser"
-
+require "beeline_matcher/helper"
---
+++ lib/beeline_matcher/helper.rb
@@ -0,0 +1,21 @@
+require 'json'
+module BeelineMatcher
+	class Helper
+		def self.parse(uri)
+			# returns a hash with a function
+			parser = BeelineHelper::Parser.new(uri)
+			parser.save_ingredients_to_file
+			parser.parse_ingredients
+			parser.convert_to_json
+
+			# read in json file
+			f = File.read(parser.json_filename)
+			hash = JSON.parse(f)
+			f.close
+
+			# clean files
+			parser.clean_files
+			return hash
+		end
+	end
+end
